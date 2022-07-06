69fa6d5f58d6929f4f44afea992f04b7f41cd061
thomasluizon <thomaslrgregorio@gmail.com>
2022-07-05 21:40:22 -0300

    fixed cors problem

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ import {
 	deleteBook,
 	deleteAuthor,
 } from './db.js';
-import express from 'express';
 
+import express from 'express';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 
